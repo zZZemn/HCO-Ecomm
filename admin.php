cdfdaa98ba3d42408616107448c33b14e2627e9a
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-11-07 19:44:40 +0800

    init

---
+++ admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+<body>
+    <div class="admin-control">
+        <ul>
+            <li><a href="#">Accounts</a></li>
+            <li><a href="#">Orders</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#">Profiles</a></li>
+            <li><a href="#">Profiles</a></li>
+            <li><a href="#">Profiles</a></li>
+        </ul>
+    </div>
+</body>
+</html>
