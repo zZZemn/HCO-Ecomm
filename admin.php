70897a604fe5388785f3b3731a2b70345964841c
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-11-07 20:08:02 +0800

    asd

--- admin.php
+++ admin.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="admin.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&family=Poppins:ital,wght@0,400;1,500&family=Source+Code+Pro:wght@300;700&display=swap" rel="stylesheet">
     <title>Admin Page</title>
 </head>
 <body>
